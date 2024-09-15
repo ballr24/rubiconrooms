3213735d84f9894f7de2b3a0307c1d4e93466cd0
ballr24 <60649021+ballr24@users.noreply.github.com>
2024-09-15 18:53:31 +0200

    test

--- add_security_headers.sh
+++ add_security_headers.sh
@@ -20,7 +20,13 @@ else
   exit 1
 fi
 
-# Insert security headers in the desired format in the `/` context
+# Check if there's a 'note' entry where 'extraHeaders' should be and remove it
+if grep -q "note" "$VHOST_CONFIG_PATH"; then
+  sed -i '/note                    <<<END_note/,/END_note/d' "$VHOST_CONFIG_PATH"
+  log_message "'note' block found and removed from $VHOST_CONFIG_PATH."
+fi
+
+# Insert security headers in the correct `/` context
 if ! grep -q "extraHeaders" "$VHOST_CONFIG_PATH"; then
   # Find the context for `/` and add extraHeaders below it
   sed -i '/context \/ {/a \  extraHeaders            <<<END_extraHeaders\nStrict-Transport-Security: max-age=31536000; includeSubDomains\nContent-Security-Policy \"upgrade-insecure-requests;connect-src *\"\nReferrer-Policy strict-origin-when-cross-origin\nX-Frame-Options: SAMEORIGIN\nX-Content-Type-Options: nosniff\nX-XSS-Protection 1;mode=block\nPermissions-Policy: geolocation=(self \"\")\n  END_extraHeaders' "$VHOST_CONFIG_PATH"
